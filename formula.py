73e299c106397a1289fea002307e7860aa1fbfed
Jason4Jason <159550090+Jason4Jason@users.noreply.github.com>
2026-01-10 22:47:28 -0500

    Carbon List
    - identify carbon emission for food on the receipt

--- formula.py
+++ formula.py
@@ -1,1 +1,14 @@
 food_file = open("food price FINAL(food price FINAL).csv", 'r')
+food_list = []
+for food in food_file:
+    current_food = food.strip().split(",")
+    food_list.append(current_food)
+
+carbon_list = []
+receipt = ["Tomatoes", "Potatoes", "Shrimp"]
+for camera_food in receipt:
+    for i in range(len(food_list)):
+        if food_list[i][0] == camera_food:
+            food_carbon = [food_list[i][0], food_list[i][2]]
+            carbon_list.append(food_carbon)
+print(carbon_list)
